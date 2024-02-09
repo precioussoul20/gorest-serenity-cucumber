0ded0fa3f680d55f982997c68e025bd2fb7efd85
pjethalia <pjethalia2005@gmail.com>
2024-02-09 17:07:37 +0000

    "Project"

---
+++ src/main/java/com/gorest/constant/Path.java
@@ -0,0 +1,12 @@
+package com.gorest.constant;
+
+public class Path {
+
+
+    // This path for User API
+    public static final String USERS = "/users";
+
+    // This path for post API
+    public static final String POSTS = "/posts";
+
+}
